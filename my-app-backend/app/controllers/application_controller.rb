6e2f943b48f6d48e38ce8b0cd925266d4c64afad
Christopher Perez <ChrisAmadeusPerez@gmail.com>
2022-11-03 01:21:43 -0400

    update controller

--- my-app-backend/app/controllers/application_controller.rb
+++ my-app-backend/app/controllers/application_controller.rb
@@ -6,9 +6,12 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  #<<<<<<<<<<<<< TEAM CRUD >>>>>>>>>>>>>>
+
   get '/teams' do
     teams = Team.all
     teams.to_json(include: [:players])
+
   end
 
   post '/teams' do
@@ -19,22 +22,79 @@ class ApplicationController < Sinatra::Base
     else
       { errors: teams.errors.full_messages }.to_json
     end
-    
+
   end
 
-  # patch '/teams/:id' do
-  #   message = Message.find(params[:id])
-  #   message.update(
-  #     body: params[:body],
-  #     username: params[:username]
-  #   )
-  #   message.to_json
-  # end
+  patch '/teams/:id' do
+    teams = Team.find(params[:id])
 
-  # delete '/teams/:id' do
-  #   message = Message.find(params[:id])
-  #   message.destroy
-  #   message.to_json
-  # end
+    teams.update(
+      name: params[:name],
+      location: params[:location],
+      league: params[:league]
+    )
+
+    teams.to_json(include: [:players])
+
+  end
+
+  delete '/teams/:id' do
+    teams = Team.find(params[:id])
+
+    teams.destroy
+
+    teams.to_json
+
+  end
+
+  #<<<<<<<<<<<<< PLAYER CRUD >>>>>>>>>>>>>>
+
+  get '/players' do
+    players = Player.all
+    players.to_json
+
+  end
+
+  post '/players' do
+    players = Player.new(params)
+
+    if players.save
+      players.to_json
+    else
+      { errors: players.errors.full_messages }.to_json
+    end
+
+  end
+
+  patch '/players/:id' do
+    players = Player.find(params[:id])
+
+    players.update(
+      name: params[:name],
+      position: params[:position],
+      games: params[:games],
+      innings: params[:innings],
+      era: params[:era],
+      win: params[:win],
+      loss: params[:loss],
+      batting_average: params[:batting_average],
+      hr: params[:hr],
+      rbi: params[:rbi],
+      jersey_number: params[:jersey_number],
+      team_id: params[:team_id]
+    )
+
+    teams.to_json
+
+  end
+
+  delete '/players/:id' do
+    players = Player.find(params[:id])
+
+    players.destroy
+
+    players.to_json
+
+  end
 
 end
